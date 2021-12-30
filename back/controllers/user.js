f38792d0f4fd1a81b81c97c8e44d3c753b992ddf
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2021-12-30 16:58:37 +0100

    modif password

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -5,19 +5,49 @@ const jwt = require('jsonwebtoken');
 const hash = require('hash.js');
 
 
+const passwordValidator = require('password-validator');
+
+
+const shemaPassValid = new passwordValidator();
+shemaPassValid
+.is().min(8)
+.is().max(50)
+.has().uppercase()
+.has().lowercase()
+.has().digits(1)
+.has().not().spaces()
+.is().not().oneOf(['Passw0rd', 'Password123', 'Pass123word']);
+
+
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-            email: req.body.email,
+  const regexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;   
+  if (!regexEmail.test(req.body.email)) {    
+      res.status(401).json({ error: "Rentrez un email valide!" })
+      return false
+  }  
+
+  const emailHash = hash.sha256().update(req.body.email).digest('hex');   // If email is validated, it is updated and digested to be hex encoded.
+
+  if (!shemaPassValid.validate(req.body.password)) {
+      res.status(401).json({message:"L'email exist déjà et/ou le mot de passe doit contenir min 1 majuscule, 1 minuscule, 1 chiffre et min de 8 caractères!"});
+
+  } else {
+    bcrypt.hash(req.body.password, 10)  
+    .then(hash => {
+        const user = new User(
+        {    
+            email: emailHash,
             password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
+        });
+ 
+    user.save()  
+        .then(() => res.status(201).json({ message: 'Nouveu utilisateur crée !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+    }
+    };
+  
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
