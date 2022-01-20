de63e5e25ecb2df6915f09275ea5922d61c7aa87
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2022-01-20 16:51:58 +0100

    modif

--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -5,11 +5,11 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');  
 
 
-router.post('', auth, multer, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.get('', auth, sauceCtrl.getAllSauces);
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 
